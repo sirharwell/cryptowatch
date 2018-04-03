445f36aa12d0575590cd229eee156baf381efe6d
Ian Harwell <sirharwell@gmail.com>
2018-04-03 10:05:40 -0600

    controller

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -1,16 +1,24 @@
 class Api::CoinsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_coin, only: [:show, :update, :destory]
+  BASE_URL = 'https://api.coinmarketcap.com/v1/ticker/'
+
   def index
+    render json: current_user.coins
   end
 
   def create
   end
 
   def show
+    render json: @coin
   end
 
   def update
+    current_user.watched_coins.find_by(coin_id: @coin.id).destroy
   end
 
   def destroy
+    @coin.destroy
   end
 end
