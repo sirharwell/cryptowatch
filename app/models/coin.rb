382fca31cb89631b01d991a9f3d19ba389c2ca8b
Ian Harwell <sirharwell@gmail.com>
2018-04-03 09:59:11 -0600

    gems

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,2 +1,7 @@
 class Coin < ApplicationRecord
+  validates_uniquenes_of :cmc_id, :name, :symbol, { case_sensitive: false }
+  validates_presence_of :cmc_id, :name, :symbol
+
+  has_many :watched_coins, dependent: :destroy
+  has_many :users, through: :watched_coins
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+has_many :watched_coins, dependent: :destroy
+  has_many :coins, through: :watched_coins
+
 end
